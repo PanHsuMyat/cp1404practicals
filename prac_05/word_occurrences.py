08ea9f75e09da2015317c6c15a2ff15d55a1bb84
PanHsuMyat <panhsumyat@my.jcu.edu.au>
2025-03-19 16:32:51 +0800

    Added word occurrences counter with sorted output

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,23 @@
+"""
+CP1404 Practical 5
+Word Occurrences
+Estimate: 20 minutes
+Actual:   37 minutes
+"""
+
+text = input("Please enter text: ")
+words = text.lower().split()
+word_to_count = {}
+
+for word in words:
+    word = word.strip('.,!?;:"\'')
+    frequency = word_to_count.get(word, 0)
+    word_to_count[word] = frequency + 1
+print(f"Text: {text}")
+
+unique_words = list(word_to_count.keys())
+unique_words.sort()
+max_word_length = max(len(word) for word in unique_words)
+
+for word in unique_words:
+    print(f"{word:{max_word_length}} : {word_to_count[word]}")
